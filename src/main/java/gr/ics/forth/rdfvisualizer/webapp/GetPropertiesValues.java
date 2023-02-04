0d6ebac46fe4a3c5d1ed2dccd1f7be7d520b1eb3
Kostas Petrakis <petrakis1@gmail.com>
2023-02-05 00:18:25 +0200

    Update GetPropertiesValues.java

--- src/main/java/gr/ics/forth/rdfvisualizer/webapp/GetPropertiesValues.java
+++ src/main/java/gr/ics/forth/rdfvisualizer/webapp/GetPropertiesValues.java
@@ -7,8 +7,6 @@ package gr.ics.forth.rdfvisualizer.webapp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
