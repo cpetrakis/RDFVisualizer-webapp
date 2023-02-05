801049bedf234b5001a4f1fbc05cba600963d30e
Kostas Petrakis <petrakis1@gmail.com>
2023-02-05 12:26:48 +0200

    Update XSTREAMpropertyReader.java

--- src/main/java/api/core/properties/XSTREAMpropertyReader.java
+++ src/main/java/api/core/properties/XSTREAMpropertyReader.java
@@ -7,9 +7,13 @@ package api.core.properties;
 
 import com.thoughtworks.xstream.XStream;
 import api.core.utils.IntPair;
+import com.thoughtworks.xstream.security.NoTypePermission;
+import com.thoughtworks.xstream.security.NullPermission;
+import com.thoughtworks.xstream.security.PrimitiveTypePermission;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +28,18 @@ public class XSTREAMpropertyReader {
 
         FileReader reader = new FileReader(filePath);  // load file
         XStream xstream = new XStream();
+        
+        //security
+        // clear out existing permissions and set own ones
+        xstream.addPermission(NoTypePermission.NONE);
+        xstream.addPermission(NullPermission.NULL);
+        xstream.addPermission(PrimitiveTypePermission.PRIMITIVES);
+        xstream.allowTypeHierarchy(Collection.class);
+        // allow any type from the same package
+        xstream.allowTypesByWildcard(new String[] {
+        "api.core.**"
+        });
+        //
         xstream.processAnnotations(Xproperties.class);
         xstream.processAnnotations(Preference.class);
         xstream.processAnnotations(WeightedProperty.class);
